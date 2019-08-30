7eb2d77ddc02638910f6165cf493ba337ec776d6
Febe <febefebrita@gmail.com>
2019-08-30 16:08:14 +0700

    week 3 - 7

--- Week2/src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ Week2/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -2,20 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../recipes.service';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.page.html',
   styleUrls: ['./recipe-detail.page.scss'],
 })
+
 export class RecipeDetailPage implements OnInit {
   loadedRecipe: Recipe;
   constructor(
     private activatedRoute: ActivatedRoute, 
     private recipeSvc: RecipesService,
     private alertController: AlertController,
-    private router: Router) { }
+    private router: Router,
+    public toastController: ToastController) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(
@@ -24,7 +26,7 @@ export class RecipeDetailPage implements OnInit {
         this.loadedRecipe = this.recipeSvc.getRecipe(paramMap.get('recipeId'));
       });
   }
-  
+
   async deleteRecipe(){
     const alert = await this.alertController.create({
       header: 'Delete Recipe?',
@@ -36,6 +38,7 @@ export class RecipeDetailPage implements OnInit {
             this.recipeSvc.deleteRecipe(this.loadedRecipe.id);
             this.router.navigate(['/recipes']);
           }
+          
         },
         {
           text: 'Cancel',
@@ -43,6 +46,11 @@ export class RecipeDetailPage implements OnInit {
         }
       ]
     });
+    // const toast = await this.toastController.create({
+    //   message: 'Recipe has been deleted.',
+    //   duration: 2000
+    // });
+    // toast.present();
     await alert.present();
   }
 }
