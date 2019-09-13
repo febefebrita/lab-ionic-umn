5148ae15b6f57a5012bc6fe40b4be9b13d2c3e6f
Febe <febefebrita@gmail.com>
2019-09-13 15:26:18 +0700

    Week 5 - 3

--- umn-homes/src/app/places/offers/offers.page.ts
+++ umn-homes/src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -10,10 +12,14 @@ import { PlacesService } from '../places.service';
 export class OffersPage implements OnInit {
   offers: Place[];
   
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService,
+    private router: Router) { }
 
   ngOnInit() {
     this.offers = this.placesService.places;
   }
-
+  editOffer(id: string, slidingItem: IonItemSliding){
+    slidingItem.close();
+    this.router.navigate(['/','places','tabs','offers','edit',id]);
+  }
 }
