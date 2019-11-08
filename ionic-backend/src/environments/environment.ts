8274f1c1ed544b3fe5df0886ee6eeff8ef2a75da
Febe <febefebrita@gmail.com>
2019-11-08 13:20:41 +0700

    week10

---
+++ ionic-backend/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB-eHIKj8crrrN0AM1p9lREzPE_VZCTIBE",
+    authDomain: "mobile-ionic-week-11.firebaseapp.com",
+    databaseURL: "https://mobile-ionic-week-11.firebaseio.com",
+    projectId: "mobile-ionic-week-11",
+    storageBucket: "mobile-ionic-week-11.appspot.com",
+    messagingSenderId: "884295335936",
+    appId: "1:884295335936:web:d266d6c3fe4500b69758e5",
+    measurementId: "G-LDLYD6Z0PT"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
